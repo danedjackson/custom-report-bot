4daf8f5f7b16cc8d696981199e062e8903bef806
unknown <danedjackson@yahoo.com>
2021-10-13 09:33:17 -0500

    Create channel on user DM

--- src/bot.js
+++ src/bot.js
@@ -2,8 +2,9 @@ require('dotenv').config();
 const { Client, Intents } = require('discord.js');
 
 const token = process.env.TOKEN;
+const guildId = process.env.GUILDID;
 
-const client = new Client( { intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]  } );
+const client = new Client( { partials: ["CHANNEL"], intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.DIRECT_MESSAGES]  } );
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}`);
@@ -11,6 +12,14 @@ client.on('ready', () => {
 
 client.on('messageCreate', async message => {
     if(message.author.bot) return;
-})
+
+    if(message.channel.type == 'DM'){
+        var server = client.guilds.cache.get(guildId);
+        server.channels.create(message.author.tag, { reason: `report for ${message.author.username}` });
+    }
+
+});
+
+
 
 client.login(token);
